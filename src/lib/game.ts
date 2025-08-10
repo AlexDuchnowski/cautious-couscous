a2fc68e7306095f13651b0229e81d74a83636b5a
AlexDuchnowski <alex_duchnowski@brown.edu>
2025-08-10 11:56:47 -0400

    export game (facepalm)

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -1,6 +1,7 @@
-import type { Grid, Vec2, Direction } from './types';
+import type { Grid, Vec2 } from './types';
+import { Direction } from './types';
 
-class Game {
+export class Game {
 	grid: Grid = [];
 	playerPosition: Vec2 = { x: 0, y: 0 };
 
@@ -18,8 +19,25 @@ class Game {
 	}
 
 	movePlayer(direction: Direction): { start: Vec2; end: Vec2; d: Direction } {
+		let startPos = this.playerPosition;
+
+		switch (direction) {
+			case Direction.Up:
+				this.playerPosition.y = Math.max(0, this.playerPosition.y - 1);
+				break;
+			case Direction.Right:
+				this.playerPosition.x = Math.min(this.grid[0].length - 1, this.playerPosition.x + 1);
+				break;
+			case Direction.Down:
+				this.playerPosition.y = Math.min(this.grid.length - 1, this.playerPosition.y + 1);
+				break;
+			case Direction.Left:
+				this.playerPosition.x = Math.max(0, this.playerPosition.x - 1);
+				break;
+		}
+
 		return {
-			start: { ...this.playerPosition },
+			start: startPos,
 			end: { x: 1, y: 2 },
 			d: direction
 		};
