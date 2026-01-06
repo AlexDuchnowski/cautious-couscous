cbbbee0274e174d59bb0250df81f685b4895008d
AlexDuchnowski <alex_duchnowski@brown.edu>
2026-01-06 08:14:00 -0500

    modify portal levels

--- src/lib/levels/stage_2.ts
+++ src/lib/levels/stage_2.ts
@@ -3,21 +3,23 @@ import { type Level, Empty as E, Wall as W, Goal as G, Portal as P } from '../ty
 export const stage_2: Level[] = [
 	{
 		grid: [
-			[new W(), new W(), new W(), new W(), new W()],
-			[new W(), new E(), new E(), new P(0, { x: 2, y: 3 }), new W()],
-			[new W(), new W(), new W(), new W(), new W()],
-			[new W(), new P(0, { x: 3, y: 1 }), new E(), new G(), new W()],
-			[new W(), new W(), new W(), new W(), new W()]
+			[new W(), new W(), new W(), new W(), new W(), new W()],
+			[new W(), new E(), new E(), new P(0, { x: 2, y: 3 }), new E(), new W()],
+			[new W(), new W(), new W(), new W(), new W(), new W()],
+			[new W(), new E(), new P(0, { x: 3, y: 1 }), new E(), new E(), new W()],
+			[new W(), new W(), new W(), new G(), new E(), new W()],
+			[new W(), new W(), new W(), new W(), new W(), new W()]
 		],
 		playerPosition: { x: 1, y: 1 }
 	},
 	{
 		grid: [
-			[new W(), new W(), new W(), new W()],
-			[new E(), new P(0, { x: 1, y: 3 }), new G(), new W()],
-			[new W(), new W(), new W(), new W()],
-			[new E(), new P(0, { x: 1, y: 1 }), new W(), new E()]
+			[new W(), new W(), new W(), new W(), new E()],
+			[new W(), new E(), new P(0, { x: 2, y: 3 }), new G(), new E()],
+			[new W(), new W(), new W(), new W(), new E()],
+			[new E(), new E(), new P(0, { x: 2, y: 1 }), new W(), new E()],
+			[new W(), new W(), new W(), new W(), new W()]
 		],
-		playerPosition: { x: 0, y: 1 }
+		playerPosition: { x: 1, y: 1 }
 	}
 ];
